0c043435eaf5ec519aff656fd123e9650a8b07e1
Kripa-Shankar <104687564+Kripa-Shankar@users.noreply.github.com>
2023-09-17 05:32:16 +0530

    Update Resturant.js

--- src/Components/Resturant.js
+++ src/Components/Resturant.js
@@ -1,33 +1,51 @@
-import React from 'react'
-import Menu from './menuApi';
-import Menucard from './Menucard';
+import React from "react";
+import Menu from "./menuApi";
+import Menucard from "./Menucard";
+
 const Resturant = () => {
-   
-    const [menuData,setmenuData] = React.useState(Menu);
+  const [menuData, setmenuData] = React.useState(Menu);
 
-    const filterItem = (category)=> {
-         const updatedList= Menu.filter((curEle) => {
+  const filterItem = (category) => {
+    const updatedList = Menu.filter((curEle) => {
+      return curEle.category === category;
+    });
+    setmenuData(updatedList);
+  };
 
-            return curEle.category ===category;
-         }); 
-         setmenuData(updatedList);
-    };
-
-    // useState hooks is used store any data 
+  function All() {
+    setmenuData(Menu);
+  }
+  // useState hooks is used store any data
   return (
-   <>
-   <nav className="navbar">
-      <div className="btn-group">
-        <button className='btn-group__item' onclick={() => filterItem("breakfast")}>Breakfast</button>
-        <button className='btn-group__item'>lunch</button>
-        <button className='btn-group__item'>Dinner</button>
-        <button className='btn-group__item'>All</button>
-      </div>
+    <>
+      <nav className="navbar">
+        <div className="btn-group">
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("breakfast")}
+          >
+            Breakfast
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("lunch")}
+          >
+            lunch
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("dinner")}
+          >
+            Dinner
+          </button>
+          <button className="btn-group__item" onClick={() => All()}>
+            All
+          </button>
+        </div>
+      </nav>
+      <Menucard menuData={menuData} />
+    </>
+  );
+};
 
-   </nav>
-   <Menucard menuData={menuData}/>
-   </>
-  )
-}
-
-export default Resturant
+export default Resturant;
